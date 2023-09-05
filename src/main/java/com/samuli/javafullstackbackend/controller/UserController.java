d90319f35cea9ccc26842937fc4f13341a74c8cd
sampet93 <47121537+sampet93@users.noreply.github.com>
2023-09-05 14:43:56 +0300

    Fix CORS error in frontend

---
+++ src/main/java/com/samuli/javafullstackbackend/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.samuli.javafullstackbackend.controller;
+
+import com.samuli.javafullstackbackend.model.User;
+import com.samuli.javafullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
