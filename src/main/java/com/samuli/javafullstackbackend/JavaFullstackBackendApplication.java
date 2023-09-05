451cf3e4d91ce91220e8cdf88eab8bc988060e6e
sampet93 <47121537+sampet93@users.noreply.github.com>
2023-09-05 11:46:52 +0300

    Init commit

---
+++ src/main/java/com/samuli/javafullstackbackend/JavaFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samuli.javafullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFullstackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuli/javafullstackbackend/JavaFullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuli.javafullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
