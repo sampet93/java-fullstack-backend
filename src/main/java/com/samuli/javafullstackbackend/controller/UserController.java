87f997922374c8ad048877eb3a37a60ce7cff54d
sampet93 <47121537+sampet93@users.noreply.github.com>
2023-09-07 14:24:45 +0300

    Add exception handling

--- src/main/java/com/samuli/javafullstackbackend/controller/UserController.java
+++ src/main/java/com/samuli/javafullstackbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.samuli.javafullstackbackend.controller;
 
+import com.samuli.javafullstackbackend.exception.UserNotFoundException;
 import com.samuli.javafullstackbackend.model.User;
 import com.samuli.javafullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 }
