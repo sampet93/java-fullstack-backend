09d6111f389c60788543541dc63fba91a810f005
sampet93 <47121537+sampet93@users.noreply.github.com>
2023-09-07 14:46:43 +0300

    Add mappings for update and delete

--- src/main/java/com/samuli/javafullstackbackend/controller/UserController.java
+++ src/main/java/com/samuli/javafullstackbackend/controller/UserController.java
@@ -4,6 +4,7 @@ import com.samuli.javafullstackbackend.exception.UserNotFoundException;
 import com.samuli.javafullstackbackend.model.User;
 import com.samuli.javafullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +29,25 @@ public class UserController {
     User getUserById(@PathVariable Long id){
         return userRepository.findById(id)
                 .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setName(newUser.getName());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User " + id + " deleted.";
     }
 }
